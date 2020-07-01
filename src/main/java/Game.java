a6b7c44ca0e5cf9cb46423268302e9d6e4d99c0f
Natalia Munina <nataly.munina@gmail.com>
2020-07-01 10:57:15 +0200

    added printBoard()

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 public class Game {
     Scanner scanner = new Scanner(System.in);
 
-
     public void start() {
+        // welcome player, give instructions
+
         greet();
 
+        //play the turns
         for (int i = 0; i < 1; i++) {
-            makeTurn();
-            saveScore();
+            // make the turn and save the score of the turn
+            saveScore(makeTurn());
+
             printScore();
         }
     }
@@ -18,11 +21,37 @@ public class Game {
     private void printScore() {
     }
 
-    private void saveScore() {
+    private void saveScore(int[] diceState) {
+        printBoard();
+
 
     }
 
-    private void makeTurn() {
+    private void printBoard() {
+        System.out.println("Select the combination");
+
+        System.out.println("1. Aces: " + "");
+        System.out.println("2. Twos: " + "");
+        System.out.println("3. Threes: " + "");
+        System.out.println("4. Fours: " + "");
+        System.out.println("5. Fives: " + "");
+        System.out.println("6. Sixes: " + "");
+        System.out.println("Sum 1-6: " + ""); //combined score of upper section
+        System.out.println("Bonus: " + ""); //if combined score is 63 or more, add 50 points in bonus
+        System.out.println("7. Two Of A Kind: " + "");
+        System.out.println("8. Two Pairs: " + "");
+        System.out.println("9. Three Of A Kind: " + "");
+        System.out.println("10. Four Of A Kind: " + "");
+        System.out.println("11. Small Straight: " + ""); // Four sequential dice (1-2-3-4, 2-3-4-5, or 3-4-5-6)
+        System.out.println("12. Big Straight: " + ""); // Five sequential dice (1-2-3-4-5 or 2-3-4-5-6)
+        System.out.println("13. Full House: " + "");
+        System.out.println("14. Chans: " + "");
+        System.out.println("15. YATZY!: " + "");
+
+
+    }
+
+    private int[] makeTurn() {
         int[] diceState = new int[5];
         int[] rerollStates = new int[]{0, 1, 2, 3, 4};
         int i = 0;
@@ -30,6 +59,8 @@ public class Game {
         do {
             //throw dice
             throwDice(diceState, rerollStates);
+
+            //show dice state
             System.out.println(Arrays.toString(diceState));
 
             System.out.print("Do you want to throw again? y or n: ");
@@ -37,8 +68,11 @@ public class Game {
             i++;
         } while (i < 2 && response == 'y');
         System.out.println(Arrays.toString(diceState));
+
+        return diceState;
     }
 
+    //generate 5 randome dice from 1 to 6
     private void throwDice(int[] diceState, int[] rerollStates) {
         for (int i = 0; i < rerollStates.length; i++) {
             diceState[i] = (int)(Math.random() * 6) + 1;
--- src/main/java/YatzyApplication.java
+++ src/main/java/YatzyApplication.java
@@ -2,6 +2,7 @@ public class YatzyApplication {
     public static void main(String[] args) {
         Game game = new Game();
 
+        // start the game
         game.start();
 
     }
